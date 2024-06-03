74f88a6386e76c557093d0ec32dfe0bc81f83736
TomasDolejsek <tdolejsek@centrum.cz>
2024-06-03 22:04:15 +0200

    Day11

--- MyGameDiary/games_app/urls.py
+++ MyGameDiary/games_app/urls.py
@@ -6,8 +6,8 @@ app_name = 'games_app'
 urlpatterns = [
     # Game views
     path('game-add/', GameAddView.as_view(), name='game_add'),
-    path('game-add/<str:game_title>/', GameSaveView.as_view(), name='game_save'),
-    path('game-detail/<int:pk>/', GameDetailView.as_view(), name='game_detail'),
+    path('game-add/<str:game_title>', GameSaveView.as_view(), name='game_save'),
+    path('game-detail/<int:pk>', GameDetailView.as_view(), name='game_detail'),
     path('game-list/', GameListView.as_view(), name='game_list'),
 
     ]
